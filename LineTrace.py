0f68315c2befbbe3d4aa6b092a57e18cc31151a3
Koki <89891689+koki0517@users.noreply.github.com>
2022-12-30 11:25:38 +0900

    LineTrace.py を更新

--- LineTrace.py
+++ LineTrace.py
@@ -21,13 +21,11 @@ timer.reset()
 
 def changeRGBtoHSV(rgb):
     rgb0_255 = rgb[0] * 255 / 1024, rgb[1] * 255 / 1024, rgb[2] * 255 / 1024
-    # defference between max of rgb and min of it
     maxRGB, minRGB = max(rgb0_255), min(rgb0_255)
 
-    # 最大値 - 最小値
     diff = maxRGB - minRGB
 
-    # Hの値を計算
+    # Hue
     if maxRGB == minRGB : hue = 0
     elif maxRGB == rgb0_255[0]:
         hue = 60 * ((rgb0_255[1] - rgb0_255[2]) / diff)
@@ -38,7 +36,7 @@ def changeRGBtoHSV(rgb):
     if hue < 0:
         hue += 360
 
-    # Sの値を計算
+    # Saturation
     if maxRGB != 0:
         saturation = diff / maxRGB * 100
     else:
